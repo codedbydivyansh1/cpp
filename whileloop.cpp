b48dd3fcefb5d47056f4f5d8bbca2d62d834347e
divyansh shukla <divyanshshukla3107@gmail.com>
2025-07-12 19:27:18 +0530

    added

--- whileloop.cpp
+++ whileloop.cpp
@@ -14,17 +14,3 @@
 // }
 // sum of n natural numbers
 // Online C++ compiler to run C++ program online
-#include <iostream>
-using namespace std;
-int main(){
-    int n=10;
-    int i;
-    int sum;
-    sum=0;
-    while(i<=n){
-        sum=sum+i;
-        cout<<sum<<" ";
-        i++;
-
-    }
-}
---
+++ whileloop1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n=10;
+    int i;
+    int sum;
+    sum=0;
+    while(i<=n){
+        sum=sum+i;
+        cout<<sum<<" ";
+        i++;
+
+    }
+}
