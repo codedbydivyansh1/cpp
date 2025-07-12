638b4e12fb6238e0743daee6188e1d73eebaae60
divyansh shukla <divyanshshukla3107@gmail.com>
2025-07-12 18:09:09 +0530

    added

---
+++ whileloop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int i=1;
+    cout<<"Enter a number: ";
+    cin>>n;
+    while(i<=n){
+        if(i%2==0){
+            cout<<i<<" ";
+
+        }i++;
+    }
+}
